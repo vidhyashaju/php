4ebb35578bbcbba09e6258f3a0c65d5061f2a267
vidhya shaju <vidhyaravindran4u@gmail.com>
2023-07-03 22:42:36 +0400

    add multiplication

---
+++ multiplication.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($a=1;$a<=10;$a++)
+    {
+        $res=$a*2;
+        echo " 2 * " .$a. "=" . $res . "<br>";
+    }
+    ?>
+</body>
+</html>
